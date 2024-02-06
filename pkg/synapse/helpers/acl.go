f2092dbc9ea0eb7c3960620aa9f49560e623dd29
Doublemme <martin.michael1997@gmail.com>
2024-02-06 17:55:32 +0100

    fix: acl sync function and impl tests

--- pkg/synapse/helpers/acl.go
+++ pkg/synapse/helpers/acl.go
@@ -2,69 +2,128 @@ package helpers
 
 import (
 	"encoding/json"
-	"os"
+	"io"
 
 	"github.com/doublemme/synapse/pkg/core/models"
 	"github.com/doublemme/synapse/pkg/synapse/types"
 	"gorm.io/gorm"
 )
 
-// Load the acl configuration from the given files
-func UnmarshalAcl(acl *[]types.AclModule, configFiles ...string) error {
+// Load the modules for the access control list from the reader into the &acl
+func UnmarshalAcl(acl *[]types.AclModule, reader io.Reader) error {
 
-	for _, aclConf := range configFiles {
-		moduleAcl := make([]types.AclModule, 0)
+	moduleAcl := make([]types.AclModule, 0)
 
-		file, err := os.OpenFile(aclConf, 0, os.ModeAppend)
-		if err != nil {
-			return err
+	decoder := json.NewDecoder(reader)
+	err := decoder.Decode(&moduleAcl)
+	if err != nil {
+		return err
+	}
+
+	*acl = append(*acl, moduleAcl...)
+
+	return nil
+}
+
+// Synchronize the ACL modules provided to the database
+//
+// return an error if something in the insert operation goes wrong
+//
+// @param db *gorm.DB - A pointer to the gorm DB instance
+//
+// @param acls *[]AclModules - A pointer to a slice of Acl modules
+func SyncAcl(db *gorm.DB, modules *[]types.AclModule) error {
+
+	// Loop all the modules and insert it if it's not found in db
+	for _, m := range *modules {
+		module, exist := getModuleByName(m.Name, db)
+		if !exist {
+
+			module = models.AuthModule{
+				Name:        m.Name,
+				Description: m.Description,
+			}
+			if err := db.Create(&module).Error; err != nil {
+				return err
+			}
 		}
 
-		decoder := json.NewDecoder(file)
-		err = decoder.Decode(&moduleAcl)
-		if err != nil {
-			return err
+		// loop throught the resources
+		for _, res := range m.Resources {
+
+			resource, exist := getResourceByName(module.Id.String(), res.Name, db)
+			if !exist {
+				resource = models.AuthResource{
+					ModuleId:    module.Id,
+					Name:        res.Name,
+					Description: res.Description,
+				}
+
+				if err := db.Create(&resource).Error; err != nil {
+					return err
+				}
+			}
+
+			// loop throught the resource actions
+			for _, act := range res.Actions {
+
+				action, exist := getActionByName(resource.Id.String(), act.Name, db)
+				if !exist {
+
+					action = models.AuthAction{
+						ResourceId:  resource.Id,
+						Name:        act.Name,
+						Description: act.Description,
+					}
+
+					if err := db.Create(&action).Error; err != nil {
+						return err
+					}
+				}
+			}
+
 		}
 
-		*acl = append(*acl, moduleAcl...)
 	}
 
 	return nil
 }
 
-func SyncAclToDb(db *gorm.DB, acls *[]types.AclModule) error {
-	// Loop all the acl modules given
-	for _, acl := range *acls {
-		module := models.AuthModule{
-			Name:        acl.Name,
-			Description: acl.Description,
-		}
+// Search for the module with the given name and return it if exist
+func getModuleByName(moduleName string, db *gorm.DB) (models.AuthModule, bool) {
+	exist := false
+	var model models.AuthModule
 
-		// loop throught the resources
-		for _, res := range acl.Resources {
-			resource := models.AuthResource{
-				ModuleId:    module.Id,
-				Name:        res.Name,
-				Description: res.Description,
-			}
-			// loop throught the resource.actions and create them
-			for _, act := range res.Actions {
-				action := models.AuthAction{
-					Name:        act.Name,
-					Description: act.Description,
-				}
-				resource.Actions = append(resource.Actions, action)
-			}
-			// Append the resource to the module resources list
-			module.Resources = append(module.Resources, resource)
-		}
-
-		// Create the module and all the resources and actions
-		if err := db.Model(&models.AuthModule{}).Create(&module).Error; err != nil {
-			return err
-		}
-
+	res := db.Model(&models.AuthModule{}).Where("name = ?", moduleName).Take(&model)
+	if res.RowsAffected == 1 {
+		exist = true
 	}
 
-	return nil
+	return model, exist
+}
+
+// Search for a resource filtered by moduleId and the resource name and return it if exists
+func getResourceByName(moduleId string, resName string, db *gorm.DB) (models.AuthResource, bool) {
+	exist := false
+	var resource models.AuthResource
+
+	res := db.Model(&models.AuthResource{}).Where("name = ? AND module_id = ?", resName, moduleId).Take(&resource)
+	if res.RowsAffected == 1 {
+		exist = true
+	}
+
+	return resource, exist
+}
+
+// Search for an resource action filtered by resource id and action name and return it if exists
+func getActionByName(resourceId string, actionName string, db *gorm.DB) (models.AuthAction, bool) {
+	exist := false
+	var action models.AuthAction
+
+	res := db.Model(&models.AuthAction{}).Where("name = ? AND resource_id = ?", actionName, resourceId).Take(&action)
+	if res.RowsAffected == 1 {
+		exist = true
+	}
+
+	return action, exist
 }
---
+++ pkg/synapse/helpers/acl_test.go
@@ -0,0 +1,21 @@
+package helpers
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/doublemme/synapse/pkg/synapse/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUnmarshalAcl(t *testing.T) {
+	expectedRes := []types.AclModule{{Name: "core", Description: "Core module resources", Resources: []types.AclResource{{Name: "general", Description: "Grant the access to the application general settings", Actions: []types.AclAction{{Name: "read", Description: "Allow the user to read the application's general settings"}, {Name: "write", Description: "Allow the user to edit the application's general settings"}}}}}}
+	var aclModules []types.AclModule
+
+	var buffer bytes.Buffer
+	_, err := buffer.Write([]byte(`[{"name": "core","description": "Core module resources","resources": [{"name": "general","description": "Grant the access to the application general settings","actions":[{"name": "read","description": "Allow the user to read the application's general settings"},{"name": "write","description": "Allow the user to edit the application's general settings"}]}]}]`))
+	assert.ErrorIs(t, err, nil, "error writing string to the buffer")
+
+	assert.Nil(t, UnmarshalAcl(&aclModules, &buffer), "error unmarshaling acl modules")
+	assert.Equal(t, expectedRes, aclModules)
+}
